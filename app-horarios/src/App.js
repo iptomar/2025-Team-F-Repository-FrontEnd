6019c8d9427f51161e380a74de2b14b66b9e2d15
Cristiane Mayabanza <152671012+itscristianee@users.noreply.github.com>
2025-03-28 19:15:07 +0000

    Navbar para permitir a navegação entre as diferentes páginas da aplicação. A barra de navegação facilita o acesso às principais seções do sistema, melhorando a usabilidade e a organização da interface.

--- app-horarios/src/App.js
+++ app-horarios/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Bom dia
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-100 min-h-screen p-6">
+      <Navbar />
+     
     </div>
   );
 }
---
+++ app-horarios/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+ 
+module.exports = withMT({
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+});
